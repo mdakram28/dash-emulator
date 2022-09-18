2b223df066bbe25ac15fb5407678cffe77ae646c
mdakram28 <mohdakram.ansari@ucalgary.ca>
2022-09-18 16:25:41 -0600

    reverted

--- scripts/dash-emulator.py
+++ scripts/dash-emulator.py
@@ -2,25 +2,13 @@
 
 import argparse
 import asyncio
-import json
 import logging
 import pathlib
 import re
 import sys
-from os.path import join
 from typing import Dict, Union
 
-<<<<<<< HEAD
-import uvloop
-from exp_common.exp_recorder import ExpWriterJson
-
-from dash_emulator_quic.config import load_config_env
-from dash_emulator_quic.log_handler import init_logging
-from dash_emulator_quic.network_manager import NetworkManager
-from dash_emulator_quic.player_factory import build_dash_player_over_quic
-=======
 from dash_emulator.player_factory import build_dash_player
->>>>>>> temp
 
 log = logging.getLogger(__name__)
 
@@ -31,14 +19,10 @@ def create_parser():
     arg_parser = argparse.ArgumentParser(description="Accept for the emulator")
     # Add here
 
-    arg_parser.add_argument("--beta", action="store_true", help="Enable BETA")
     arg_parser.add_argument("--proxy", type=str, help='NOT IMPLEMENTED YET')
-    arg_parser.add_argument("--plot", required=False, default=None, type=str, help="The folder to save plots")
-    arg_parser.add_argument("--env", required=False, default=None, type=str, help="Environment to use")
-    arg_parser.add_argument("--run-id", required=False, type=str, help="Run ID")
-    arg_parser.add_argument("--run-dir", required=False, type=str, help="Run Directory")
-    arg_parser.add_argument("--bw-profile", required=False, default=None, type=str, help="Bandwidth profile file path")
-    arg_parser.add_argument("--config-file", required=False, default=None, type=str, help="Load config from file")
+    arg_parser.add_argument("--output", type=str, required=False, default=None,
+                            help="Path to output folder. Indicate this argument to save videos and related data.")
+    arg_parser.add_argument("--plot", required=False, default=False, action='store_true')
     arg_parser.add_argument("-y", required=False, default=False, action='store_true',
                             help="Automatically overwrite output folder")
     arg_parser.add_argument(PLAYER_TARGET, type=str, help="Target MPD file link")
@@ -48,11 +32,7 @@ def create_parser():
 def validate_args(arguments: Dict[str, Union[int, str, None]]) -> bool:
     # Validate target
     # args.PLAYER_TARGET is required
-<<<<<<< HEAD
-    if "target" not in arguments and arguments.get("config_file") is None:
-=======
     if "target" not in arguments:
->>>>>>> temp
         log.error("Argument \"%s\" is required" % PLAYER_TARGET)
         return False
     # HTTP or HTTPS protocol
@@ -66,13 +46,8 @@ def validate_args(arguments: Dict[str, Union[int, str, None]]) -> bool:
     # TODO
 
     # Validate Output
-<<<<<<< HEAD
-    if arguments["plot"] is not None:
-        path = pathlib.Path(arguments['plot'])
-=======
     if arguments["output"] is not None:
         path = pathlib.Path(arguments['output'])
->>>>>>> temp
         path.mkdir(parents=True, exist_ok=True)
 
     return True
@@ -84,21 +59,10 @@ if __name__ == '__main__':
     except AssertionError:
         print("Python 3.3+ is required.")
         exit(-1)
-
     parser = create_parser()
     args = parser.parse_args()
 
     args = vars(args)
-
-    if args["config_file"] is not None:
-        with open(args['config_file']) as f:
-            config = json.load(f)
-        args["run_id"] = args["run_id"] or config["runId"]
-        args["run_dir"] = args["run_dir"] or config["runDir"]
-        args["bw_profile"] = args["bw_profile"] or config["bwProfile"]
-        args["beta"] = config["beta"]
-        args["target"] = args["target"] or config["target"]
-        args["env"] = args["env"] or config["env"]
 
     validated = validate_args(args)
 
@@ -106,38 +70,8 @@ if __name__ == '__main__':
         log.error("Arguments validation error, exit.")
         exit(-1)
 
-<<<<<<< HEAD
-    init_logging(run_id=args["run_id"])
-=======
     logging.basicConfig(level=logging.INFO)
 
     player = build_dash_player()
->>>>>>> temp
 
-    (player_config, downloader_config) = load_config_env(args['env'])
-
-    uvloop.install()
-
-
-    async def main():
-        event_logs = ExpWriterJson(join(args['run_dir'], "event_logs.txt"))
-        player, analyzer = build_dash_player_over_quic(
-            player_config,
-            downloader_config,
-            beta=args["beta"],
-            plot_output=args["plot"],
-            event_logs=event_logs,
-            run_dir=args["run_dir"])
-
-        # player = build_dash_player()
-        network_manager = NetworkManager(bw_profile_path=args['bw_profile'], recorder=event_logs)
-        network_manager.start_bg()
-
-        await player.start(args["target"])
-        analyzer.save(sys.stdout)
-
-        network_manager.stop_bg()
-        # await asyncio.sleep(1000000)
-
-
-    asyncio.run(main())
+    asyncio.run(player.start(args["target"]))
